4ccb88eee1f10ff122ff93f2bc885066015dafa5
Aethiree <mayooran.mahadevan@gmail.com>
2026-09-17 22:34:35 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "firstyear-v5";
+const CACHE = "firstyear-v6";
 const SHELL = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./icon-512-maskable.png", "./apple-touch-icon.png"];
 
 self.addEventListener("install", (e) => {
